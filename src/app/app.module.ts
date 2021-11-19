c5a5efd5324aadd756adef6f35d61cb8a0df6322
AzureAD\EstebanArias <estebasn.arias@ias.com.co>
2021-11-19 10:28:59 -0500

    agregando componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegistrationPageComponent,
+    ProfilePageComponent,
   ],
   imports: [
     BrowserModule,
